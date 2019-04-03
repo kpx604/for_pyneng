0c1990ac4b1882fb32a9071066161b94ea5fb9ed
kpx604 <kpx604@gmail.com>
2019-04-03 14:26:34 +0000

    4.7 DONE

--- exercises/04_data_structures/task_4_7.py
+++ exercises/04_data_structures/task_4_7.py
@@ -8,4 +8,11 @@
 
 '''
 
-MAC = 'AAAA:BBBB:CCCC'
+MAC = '2A4F:0000:12FF'
+MAC = MAC.lower()
+mac_list = MAC.split(':')
+mac_list = ''.join(mac_list)
+mac_byte = '{:b}'.format(int(mac_list,16))
+print(mac_list)
+print(mac_byte)
+
